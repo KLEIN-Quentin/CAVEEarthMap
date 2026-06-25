83a975271acd729d980d100fd7ec8c61712bde6b
Quentin Klein <kleinq@ad.unistra.fr>
2026-06-25 15:03:59 +0200

    Added Fly-To UI

--- cave-test/Assets/Scripts/Cesium/CesiumCaveInput.cs
+++ cave-test/Assets/Scripts/Cesium/CesiumCaveInput.cs
@@ -1,6 +1,9 @@
 using CesiumForUnity;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class CesiumCaveInput : MonoBehaviour
 {
@@ -31,6 +34,13 @@ public class CesiumCaveInput : MonoBehaviour
     private GameObject mainCam;
     [SerializeField]
     private Transform initialCamPosition;
+
+    [SerializeField]
+    private CesiumFlyToController flyToController;
+    [SerializeField]
+    private TMP_InputField longitudeInput;
+    [SerializeField]
+    private TMP_InputField latitudeInput;
 
     private void Awake()
     {
@@ -114,6 +124,74 @@ public class CesiumCaveInput : MonoBehaviour
         Vector3 moveDirection = leftHand.right * moveInputs.x + leftHand.forward * moveInputs.y;
         moveDirection *= RelativeSpeed();
         rb.AddForce(moveDirection, ForceMode.Acceleration);
+    }
+
+    public void FlyTo()
+    {
+        if (string.IsNullOrEmpty(longitudeInput.text))
+        {
+            Debug.LogError("Empty longitude field!");
+            return;
+        }
+        if (string.IsNullOrEmpty(latitudeInput.text))
+        {
+            Debug.LogError("Empty latitude field!");
+            return;
+        }
+        bool longitudeRelative = false;
+        bool addToLongitude = true;
+        bool latitudeRelative = false;
+        bool addToLatitude = true;
+        if (longitudeInput.text[0] == '+' || longitudeInput.text[0] == '-')
+        {
+            longitudeRelative = true;
+            addToLongitude = longitudeInput.text[0] == '+' ? true: false;
+            longitudeInput.text.Remove(0, 1);
+        }
+        if (latitudeInput.text[0] == '+' || latitudeInput.text[0] == '-')
+        {
+            latitudeRelative = true;
+            addToLatitude = latitudeInput.text[0] == '+' ? true : false;
+            latitudeInput.text.Remove(0, 1);
+
+        }
+        double destLongitude = 0f;
+        bool success = double.TryParse(longitudeInput.text, out destLongitude);
+        if (!success)
+        {
+            Debug.LogError("Invalid longitude field: must only contain digits and a dot (.)");
+            return;
+        }
+        Debug.Log("Got destLongitude = " + destLongitude);
+        double destLatitude = 0f;
+        success = double.TryParse(latitudeInput.text, out destLatitude);
+        if (!success)
+        {
+            Debug.LogError("Invalid latitude field: must only contain digits and a dot (.)");
+            return;
+        }
+        if (destLongitude < -180 || destLongitude > 180)
+        {
+            Debug.LogError("Invalid longitude field: value must be in range [-180; 180]");
+            return;
+        }
+        if (destLatitude < -90 || destLatitude > 90)
+        {
+            Debug.LogError("Invalid latitude field: value must be in range [-90; 90]");
+        }
+        Debug.Log("Got destLatitude = " + destLatitude);
+        if (longitudeRelative)
+        {
+            destLongitude = addToLongitude ? CesiumGeoRef.longitude + destLongitude : CesiumGeoRef.longitude - destLongitude;
+            Debug.Log("destLongitude was relative, it is now " + destLongitude);
+        }
+        if (latitudeRelative)
+        {
+            destLatitude = addToLatitude ? CesiumGeoRef.latitude + destLatitude : CesiumGeoRef.latitude - destLatitude;
+            Debug.Log("destLatitude was relative, it is now" + destLatitude);
+        }
+        Unity.Mathematics.double3 destination = new Unity.Mathematics.double3(destLongitude, destLatitude, CesiumGeoRef.height);
+        flyToController.FlyToLocationLongitudeLatitudeHeight(destination, transform.rotation.z, transform.rotation.y, true);
     }
 
     private void ApplyRotate()
