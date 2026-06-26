110337e52b57213cab4f9d9a90e84e67fcbcbaba
Quentin KLEIN <quentin.klein.2005@free.fr>
2026-06-26 11:55:31 +0200

    Removed UI, changed to simple button press to travel to set spots

--- cave-test/Assets/Scripts/Cesium/CesiumCaveInput.cs
+++ cave-test/Assets/Scripts/Cesium/CesiumCaveInput.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CesiumCaveInput : MonoBehaviour
 {
@@ -29,6 +30,8 @@ public class CesiumCaveInput : MonoBehaviour
 
     [SerializeField]
     private Transform leftHand;
+    [SerializeField]
+    private Transform rightHand;
 
     [SerializeField]
     private GameObject mainCam;
@@ -41,6 +44,10 @@ public class CesiumCaveInput : MonoBehaviour
     private TMP_InputField longitudeInput;
     [SerializeField]
     private TMP_InputField latitudeInput;
+
+    [SerializeField]
+    private List<CesiumGlobeAnchor> locations;
+    private int currentLocationIndex = 0;
 
     private void Awake()
     {
@@ -117,6 +124,16 @@ public class CesiumCaveInput : MonoBehaviour
         scaleDown = context.ReadValue<float>();
     }
 
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentLocationIndex = (currentLocationIndex + 1) % locations.Count;
+            Debug.Log("Flying to " + locations[currentLocationIndex].gameObject.name);
+            FlyTo(locations[currentLocationIndex].longitude, locations[currentLocationIndex].latitude, locations[currentLocationIndex].height);
+        }
+    }
+
     private void ApplyMove()
     {
         //CAVE.transform.localPosition += new Vector3(moveInputs.x / 2, 0, moveInputs.y / 2);
@@ -126,71 +143,9 @@ public class CesiumCaveInput : MonoBehaviour
         rb.AddForce(moveDirection, ForceMode.Acceleration);
     }
 
-    public void FlyTo()
+    public void FlyTo(double destLongitude, double destLatitude, double destHeight)
     {
-        if (string.IsNullOrEmpty(longitudeInput.text))
-        {
-            Debug.LogError("Champ longitude vide!");
-            return;
-        }
-        if (string.IsNullOrEmpty(latitudeInput.text))
-        {
-            Debug.LogError("Champ latitude vide!");
-            return;
-        }
-        bool longitudeRelative = false;
-        bool addToLongitude = true;
-        bool latitudeRelative = false;
-        bool addToLatitude = true;
-        if (longitudeInput.text[0] == '+' || longitudeInput.text[0] == '-')
-        {
-            longitudeRelative = true;
-            addToLongitude = longitudeInput.text[0] == '+' ? true: false;
-            longitudeInput.text.Remove(0, 1);
-        }
-        if (latitudeInput.text[0] == '+' || latitudeInput.text[0] == '-')
-        {
-            latitudeRelative = true;
-            addToLatitude = latitudeInput.text[0] == '+' ? true : false;
-            latitudeInput.text.Remove(0, 1);
-
-        }
-        double destLongitude = 0f;
-        bool success = double.TryParse(longitudeInput.text, out destLongitude);
-        if (!success)
-        {
-            Debug.LogError("Champ longitude invalide: ne doit contenir que des chiffres, et une virgule pour la décimale (,)");
-            return;
-        }
-        Debug.Log("Got destLongitude = " + destLongitude);
-        double destLatitude = 0f;
-        success = double.TryParse(latitudeInput.text, out destLatitude);
-        if (!success)
-        {
-            Debug.LogError("Champ latitude invalide: ne doit contenir que des chiffres, et une virgule pour la décimale (,)");
-            return;
-        }
-        if (destLongitude < -180 || destLongitude > 180)
-        {
-            Debug.LogError("Champ longitude invalide: doit être dans l'intervale [-180; 180]");
-            return;
-        }
-        if (destLatitude < -90 || destLatitude > 90)
-        {
-            Debug.LogError("Champ latitude invalide: doit être dans l'intervale [-90; 90]");
-        }
-        Debug.Log("Got destLatitude = " + destLatitude);
-        if (longitudeRelative)
-        {
-            destLongitude = addToLongitude ? CesiumGeoRef.longitude + destLongitude : CesiumGeoRef.longitude - destLongitude;
-            Debug.Log("destLongitude was relative, it is now " + destLongitude);
-        }
-        if (latitudeRelative)
-        {
-            destLatitude = addToLatitude ? CesiumGeoRef.latitude + destLatitude : CesiumGeoRef.latitude - destLatitude;
-            Debug.Log("destLatitude was relative, it is now" + destLatitude);
-        }
-        Unity.Mathematics.double3 destination = new Unity.Mathematics.double3(destLongitude, destLatitude, CesiumGeoRef.height);
+        Unity.Mathematics.double3 destination = new Unity.Mathematics.double3(destLongitude, destLatitude, destHeight);
         flyToController.FlyToLocationLongitudeLatitudeHeight(destination, transform.rotation.z, transform.rotation.y, true);
     }
 
