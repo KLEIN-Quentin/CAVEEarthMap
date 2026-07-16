c5e989326cf1296f6912a48ecaa592831166f896
Quentin Klein <kleinq@ad.unistra.fr>
2026-07-16 15:25:10 +0200

    Fixed UI loading not working

--- cave-test/Assets/Scripts/Cesium/UIManager.cs
+++ cave-test/Assets/Scripts/Cesium/UIManager.cs
@@ -13,9 +13,27 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private CesiumWebMapServiceRasterOverlay WMS;
 
+    private const string zeroWidthSpace = "\u200b";
+
     public void ChangeWMSUrl()
     {
-        WMS.baseUrl = InputUrl.text;
-        WMS.layers = InputLayer.text;
+        string url = InputUrl.text;
+        string layers = InputLayer.text;
+        Debug.Log("Changed URL to " + "\n" + url);
+        Debug.Log("Changed layers to " + "\n" + layers);
+        if (url.EndsWith(zeroWidthSpace))
+        {
+            url = url.Remove(url.Length - 1);
+            Debug.Log("Removed garbo char in URL");
+        }
+        if (layers.EndsWith(zeroWidthSpace))
+        {
+            layers = layers.Remove(layers.Length - 1);
+            Debug.Log("Removed garbo char in layers");
+        }
+        Debug.Log("Final URL to " + "\n" +  url);
+        Debug.Log("Final layers to " + "\n" + layers);
+        WMS.baseUrl = url;
+        WMS.layers = layers;
     }
 }
